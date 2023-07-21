136ee71238d94b83d67ea6b1ab1cd9e4a556cb85
Lucas_Aballay <lucas.aballay.2000@gmail.com>
2023-07-20 22:07:13 -0300

    arreglado el responsive de los iframes de tracks

--- src/components/TracksLanding/TracksLanding.jsx
+++ src/components/TracksLanding/TracksLanding.jsx
@@ -9,60 +9,70 @@ function TracksLanding() {
       </div>
       <div className="iframesContainer">
         <Iframe
+          className="iframe"
           url="https://bandcamp.com/EmbeddedPlayer/album=902096618/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/track=1324750353/transparent=true/"
           width="258px"
           height="400px"
           frameBorder={0}
         />
         <Iframe
+          className="iframe"
           url="https://bandcamp.com/EmbeddedPlayer/album=902096618/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/track=1324750353/transparent=true/"
           width="258px"
           height="400px"
           frameBorder={0}
         />
         <Iframe
+          className="iframe"
           url="https://bandcamp.com/EmbeddedPlayer/album=222335900/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/track=1960869958/transparent=true/"
           width="258px"
           height="400px"
           frameBorder={0}
         />
         <Iframe
+          className="iframe"
           url="https://bandcamp.com/EmbeddedPlayer/album=222335900/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/track=3970650530/transparent=true/"
           width="258px"
           height="400px"
           frameBorder={0}
         />
         <Iframe
+          className="iframe"
           url="https://bandcamp.com/EmbeddedPlayer/album=222335900/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/track=3195922246/transparent=true/"
           width="258px"
           height="400px"
           frameBorder={0}
         />
         <Iframe
+          className="iframe"
           url="https://bandcamp.com/EmbeddedPlayer/album=222335900/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/track=2200633402/transparent=true/"
           width="258px"
           height="400px"
           frameBorder={0}
         />
         <Iframe
+          className="iframe"
           url="https://bandcamp.com/EmbeddedPlayer/album=2355660743/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/track=4086598237/transparent=true/"
           width="258px"
           height="400px"
           frameBorder={0}
         />
         <Iframe
+          className="iframe"
           url="https://bandcamp.com/EmbeddedPlayer/album=2355660743/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/track=3014891731/transparent=true/"
           width="258px"
           height="400px"
           frameBorder={0}
         />
         <Iframe
+          className="iframe"
           url="https://bandcamp.com/EmbeddedPlayer/album=1144548600/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/track=819764854/transparent=true/"
           width="258px"
           height="400px"
           frameBorder={0}
         />
         <Iframe
+          className="iframe"
           url="https://bandcamp.com/EmbeddedPlayer/album=1144548600/size=large/bgcol=333333/linkcol=ffffff/tracklist=false/track=3958817297/transparent=true/"
           width="258px"
           height="400px"
